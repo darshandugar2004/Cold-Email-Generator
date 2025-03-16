b53a08b7af921de7b3140bb91ab1bd54e8fbe2a3
darshandugar2004 <darsind108@gmail.com>
2025-03-17 02:39:58 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,42 @@
+import streamlit as st
+from langchain_community.document_loaders import WebBaseLoader
+
+from chains import Chain
+from portfolio import Portfolio
+from utils import clean_text
+
+
+def create_streamlit_app(llm, portfolio, clean_text):
+    st.title("📧 Cold Mail Generator")
+    url_input = st.text_input("Enter a JOB URL:")
+    submit_button = st.button("Generate Email")
+
+    if submit_button:
+        try:
+            # Data extration form Careers Websitr
+            loader = WebBaseLoader([url_input])
+            
+                # data cleaning
+            data = clean_text(loader.load().pop().page_content)
+
+            # Read csv
+            portfolio.load_portfolio()
+
+            # call llm from chains
+            jobs = llm.extract_jobs(data)
+            for job in jobs:
+                skills = job.get('skills', [])
+                links = portfolio.query_links(skills)
+                email = llm.write_mail(job, links)
+                st.code(email, language='markdown')
+        except Exception as e:
+            st.error(f"An Error Occurred: {e}")
+
+
+if __name__ == "__main__":
+    chain = Chain()
+    portfolio = Portfolio()
+    st.set_page_config(layout="wide", page_title="Cold Email Generator", page_icon="📧")
+    create_streamlit_app(chain, portfolio, clean_text)
+
+
